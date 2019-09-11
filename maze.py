402f2f08dfbab0b00b5256314d0c1fef47540cd1
jhaj <jhaj@itu.dk>
2019-09-11 14:43:38 +0200

    maze 1

--- maze.py
+++ maze.py
@@ -7,33 +7,37 @@ from time import sleep
 # Connect light sensor to input 1 and 4
 lsWh = LightSensor('in1')
 lsBl = LightSensor('in4')
+lsM = LightSensor('in2')
 
 mB = LargeMotor('outB')
 mC = LargeMotor('outC')
 
-# Put the Mode_reflect to "Reflect"
+# Put the Mode_reflect to "Reflect"01
 lsWh.MODE_REFLECT = 'REFLECT'
 lsBl.MODE_REFLECT = 'REFLECT'
+lsM.MODE_REFLECT = 'REFLECT'
 
 Loop = 10000
 
 for a in range(0, Loop):
     valueWh = lsWh.value()
     valueBl = lsBl.value()
+    valueM = lsM.value()
 
-    mB.run_forever(speed_sp= -300)
-    mC.run_forever(speed_sp= -300)
+    mB.run_forever(speed_sp= -400)
+    mC.run_forever(speed_sp= -400)
 
+    #print (valueWh)
+    #sleep(2)
+#on line: 441
+#on edge: 500
+#off line: 540
 
-    if valueWh < 480 :
+    if valueWh < 480 and valueBl < 388 :
+        mB.run_forever(speed_sp= -150)
+
+    if valueWh < 478 :
         mB.run_forever(speed_sp= 300)
 
-    if valueBl < 390 :
+    if valueBl < 388 :
         mC.run_forever(speed_sp= 300)
-
-
-
-
-
-
-
