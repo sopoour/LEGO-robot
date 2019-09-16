f8a021893f7a0157cc408883a75b2c3737687a2c
sopoour <sophia.auer@googlemail.com>
2019-09-16 10:37:43 +0200

    updated maze

--- maze.py
+++ maze.py
@@ -4,40 +4,68 @@ from ev3dev.ev3 import *
 from ev3dev2.sensor.lego import LightSensor
 from time import sleep
 
-# Connect light sensor to input 1 and 4
-lsWh = LightSensor('in1')
-lsBl = LightSensor('in4')
-lsM = LightSensor('in2')
 
-mB = LargeMotor('outB')
-mC = LargeMotor('outC')
+def loop (lsWh, lsBl, lsM, mBl, mWh) :
 
-# Put the Mode_reflect to "Reflect"01
-lsWh.MODE_REFLECT = 'REFLECT'
-lsBl.MODE_REFLECT = 'REFLECT'
-lsM.MODE_REFLECT = 'REFLECT'
+    Loop = 10000
 
-Loop = 10000
+    for a in range(0, Loop):
+        valueWh = lsWh.value()
+        valueBl = lsBl.value()
+        valueM = lsM.value()
 
-for a in range(0, Loop):
-    valueWh = lsWh.value()
-    valueBl = lsBl.value()
-    valueM = lsM.value()
+        mBl.run_forever(speed_sp= 300)
+        mWh.run_forever(speed_sp= 300)
 
-    mB.run_forever(speed_sp= -400)
-    mC.run_forever(speed_sp= -400)
+        #print (valueWh)
+        #sleep(2)
+        #on line: 441
+        #on edge: 500
+        #off line: 540
 
-    #print (valueWh)
-    #sleep(2)
-#on line: 441
-#on edge: 500
-#off line: 540
+        if (valueWh < 480 and valueM < 445) or (valueM < 445 and valueBl < 388) :
+            intersection(valueWh, valueBl, valueM, mWh, mBl)
 
-    if valueWh < 480 and valueBl < 388 :
-        mB.run_forever(speed_sp= -150)
+        followLine(valueWh, valueBl)
+
+
+#def planner (valueWh, valueBl, valueM, dir):
+
+def intersection (valueWh, valueBl, valueM, mWh, mBl):
+    if valueWh < 480 and valueBl < 388 and valueM < 445 :
+        mBl.run_forever(speed_sp= 300)
+        mWh.run_forever(speed_sp= 300)
+
+    if valueWh < 480 and valueM < 445 :
+        mWh.run_forever(speed_sp=-200)
+
+    if valueM < 445 and valueBl < 388 :
+        mBl.run_forever(speed_sp=-200)
+
+def followLine (valueWh, valueBl) :
+    #if valueWh < 480 and valueBl < 388 :
+     #   mBl.run_forever(speed_sp= 150)
 
     if valueWh < 478 :
-        mB.run_forever(speed_sp= 300)
+        mBl.run_forever(speed_sp= -300)
 
     if valueBl < 388 :
-        mC.run_forever(speed_sp= 300)
+        mWh.run_forever(speed_sp= -300)
+
+
+if __name__ == '__main__':
+    # Connect light sensor to input 1 and 4
+    lsWh = LightSensor('in4')
+    lsBl = LightSensor('in1')
+    lsM = LightSensor('in2')
+    lsT = TouchSensor('in3')
+
+    mBl = LargeMotor('outB')
+    mWh = LargeMotor('outC')
+
+    # Put the Mode_reflect to "Reflect"01
+    lsWh.MODE_REFLECT = 'REFLECT'
+    lsBl.MODE_REFLECT = 'REFLECT'
+    lsM.MODE_REFLECT = 'REFLECT'
+
+    loop(lsBl, lsWh, lsM, mBl, mWh)
