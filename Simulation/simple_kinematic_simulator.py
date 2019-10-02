9a44b1f93412adc731615db53b87cf200f7c6dbe
sopoour <sophia.auer@googlemail.com>
2019-10-02 09:41:32 +0200

    changed structure

---
+++ Simulation/simple_kinematic_simulator.py
@@ -0,0 +1,76 @@
+import shapely
+from shapely.geometry import LinearRing, LineString, Point
+from numpy import sin, cos, pi, sqrt
+from random import random
+
+# Constants
+###########
+R = 0.02  # radius of wheels in meters
+L = 0.10  # distance between wheels in meters
+
+W = 2.0  # width of arena
+H = 2.0  # height of arena
+
+robot_timestep = 0.1        # 1/robot_timestep equals update frequency of robot
+simulation_timestep = 0.01  # timestep in kinematics sim (probably don't touch..)
+
+# the world is a rectangular arena with width W and height H
+world = LinearRing([(W/2,H/2),(-W/2,H/2),(-W/2,-H/2),(W/2,-H/2)])
+
+# Variables 
+###########
+
+x = 0.0   # robot position in meters - x direction
+y = 0.0   # robot position in meters - y direction
+q = 0.0   # robot heading with respect to x-axis in radians 
+
+left_wheel_velocity =  random()   # robot left wheel velocity in radians/s
+right_wheel_velocity =  random()  # robot right wheel velocity in radians/s
+
+# Kinematic model
+#################
+# updates robot position and heading based on velocity of wheels and the elapsed time
+# the equations are a forward kinematic model of a two-wheeled robot - don't worry just use it
+def simulationstep():
+    global x, y, q
+
+    for step in range(int(robot_timestep/simulation_timestep)):     #step model time/timestep times
+        v_x = cos(q)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2) 
+        v_y = sin(q)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2)
+        omega = R*left_wheel_velocity/L - R*right_wheel_velocity/L    
+    
+        x += v_x * simulation_timestep
+        y += v_y * simulation_timestep
+        q += omega * simulation_timestep
+
+# Simulation loop
+#################
+file = open("trajectory.dat", "w")
+
+for cnt in range(5000):
+    #simple single-ray sensor
+    ray = LineString([(x, y), (x+cos(q)*2*W,y+sin(q)*2*H) ])  # a line from robot to a point outside arena in direction of q
+    s = world.intersection(ray)
+    distance = sqrt((s.x-x)**2+(s.y-y)**2)                    # distance to wall
+    
+    #simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
+    if cnt%100==0:
+        if (distance < 0.5):
+            left_wheel_velocity = -0.4
+            right_wheel_velocity = 0.4
+        else:                
+            left_wheel_velocity = random()
+            right_wheel_velocity = random()
+        
+    #step simulation
+    simulationstep()
+
+    #check collision with arena walls 
+    if (world.distance(Point(x,y))<L/2):
+        break
+        
+    if cnt%50==0:
+        file.write( str(x) + ", " + str(y) + ", " + str(cos(q)*0.05) + ", " + str(sin(q)*0.05) + "\n")
+
+file.close()
+    
