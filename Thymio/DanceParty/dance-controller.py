03778b0a649510ad9446f5267321abf5051d8a8b
brndsh <bryndis_eva@hotmail.com>
2019-10-31 16:44:56 +0100

    created functions for robot dance simulation added comments for what is missing

--- Thymio/DanceParty/dance-controller.py
+++ Thymio/DanceParty/dance-controller.py
@@ -2,6 +2,8 @@ import shapely
 from shapely.geometry import LinearRing, LineString, Point
 from numpy import sin, cos, pi, sqrt
 from random import random
+import time
+import random
 #run using ctrl + alt + N
 
 # Constants
@@ -38,40 +40,162 @@ def simulationstep():
     for step in range(int(robot_timestep/simulation_timestep)):     #step model time/timestep times
         v_x = cos(q)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2) 
         v_y = sin(q)*(R*left_wheel_velocity/2 + R*right_wheel_velocity/2)
-        omega = R*right_wheel_velocity/L - left_wheel_velocity/L    
+        omega = R*left_wheel_velocity/L - R*right_wheel_velocity/L    
     
         x += v_x * simulation_timestep
         y += v_y * simulation_timestep
         q += omega * simulation_timestep
 
+# Communication
+##############
+#Todo: change
+def startCommunication(distance_sensor_front, distance_sensor_right):
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+#Todo: change
+def acknowledgeCommunication(distance_sensor_front, distance_sensor_right):
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+#Todo: change
+def shutDownCommunication(distance_sensor_front, distance_sensor_right):
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+#Todo: change
+#return true if asked to dance
+def checkIfAskedToDance(distance_sensor_front, distance_sensor_right):
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+# Behaviours 
+############
+
+#Choose random gender
+##############
+#0 = male = blue
+#1 = female = red
+def chooseGender():
+    return random.randint(0,1)
+    
+
+# The robots randomly decide on a gender and change their color to reﬂect their gender 
+# (blue, red) 1. The robot is shy at the moment and stays still along the wall. 
+# It timidly awaits another robot to ask it to dance.
+def benchWarmer(distance_sensor_front, distance_sensor_right):
+    #Another robot can ask it to dnace
+    startCommunication(distance_sensor_front, distance_sensor_right)
+    isSingle = True
+    start = time.time()
+    end = time.time()
+    gender = chooseGender()
+    while(isSingle and (start-end <= 30)):
+        end = time.time()
+        if(checkIfAskedToDance()):
+            isSingle = False
+        print(gender)
+    isSingle = True
+    returnToRest(distance_sensor_front, distance_sensor_right)
+    #If it gets an acknowledgement it transistions to dance
+    #dance()
+
+# Once in a while the robot has built up enough courage and starts navigating along the wall 
+# counter clock-wise and around any robots in its way. If it locates a robot of the opposite 
+# gender it asks it to dance, which is always accepted because robots are nice. 
+# If it has not found a partner for a while it settles down again.
+#Todo
+#Stay further from wall?
+#Detect other robots
+#If opposite gender ask to dance
+#If a partner with the oppossite gender is detected, 
+#it communicates acknowledge ﬁve times after which it transistions to dance.
+
+def findDancePartner(cnt, distance_sensor_front):
+    global left_wheel_velocity, right_wheel_velocity
+    start = time.time()
+    end = time.time()
+    while(end-start <= 30):
+        if cnt%100==0:   
+            if (distance_sensor_front < 0.5):
+                left_wheel_velocity = L*pi/4
+                right_wheel_velocity = -L*pi/4
+            else:                
+                left_wheel_velocity = 10/(2*pi)
+                right_wheel_velocity = 10/(2*pi)
+        end = time.time()
+    #If it has not found a robot for a while (30sec) it settles down again
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+
+#Todo 
+#Turn purple
+#Navigate to the center of the arena 
+#Do a wild dance with robot
+def danceMoves():
+    global left_wheel_velocity, right_wheel_velocity, x, y
+    start = time.time()
+    end = time.time()
+    #Navigate to the center of the arena
+    #Todo: change
+    x = 0.0
+    y = 0.0
+    #Dance for 15 seconds
+    while(end-start <= 15):
+        #Dance
+        left_wheel_velocity = -L*pi/4
+        right_wheel_velocity = L*pi/4
+        end = time.time()
+    returnToRest(distance_sensor_front, distance_sensor_right)
+
+# Todo
+# The robots return to the perimeter of the arena and ﬁnd a free spot away from other robots 
+# and faces the arena
+def returnToRest(distance_sensor_front, distance_sensor_right):
+    global left_wheel_velocity, right_wheel_velocity
+    #Go to the perimeter of the arena
+    while(distance_sensor_front > 0.15):
+        left_wheel_velocity = 10/(2*pi)
+        right_wheel_velocity = 10/(2*pi)
+    
+    #Face arena
+    faceArena(distance_sensor_right)
+    left_wheel_velocity = 0
+    right_wheel_velocity = 0
+
+def faceArena(distance_sensor_right):
+    global left_wheel_velocity, right_wheel_velocity
+    #Turn 180°
+    #Todo: check if correct
+    while(distance_sensor_right > 0.15):
+        left_wheel_velocity = L*pi/4
+        right_wheel_velocity = -L*pi/4
+
+# Reading Sensor values
+#######################
+def distanceSensorFront():
+    global left_wheel_velocity, x, y
+    ray_sensor_front = LineString([(x, y), (x + cos(q) * 2 * W, y + sin(q) * 2 * H)])  
+    sensor_front = world.intersection(ray_sensor_front)
+    distance_sensor_front = sqrt(((sensor_front.x - x) ** 2 + (sensor_front.y - y) ** 2))
+    return distance_sensor_front
+
+def distanceSensorRight():
+    global right_wheel_velocity, x, y
+    ray_sensor_right = LineString([(x, y), (x + cos(q + (pi / 3)) * 2 * W, y + sin(q + (pi / 3)) * 2 * H)])  
+    sensor_right = world.intersection(ray_sensor_right)
+    distance_sensor_right = sqrt(((sensor_right.x - x) ** 2 + (sensor_right.y - y) ** 2))
+    return distance_sensor_right
+
 # Simulation loop
 #################
-file = open("trajectory.dat", "w")
-fileSen = open("sensor.dat", "w")
+file = open("./DanceParty/trajectory.dat", "w")
+fileSen = open("./DanceParty/sensor.dat", "w")
 for cnt in range(5000):
-    # simple single-ray sensor
-    # a line from robot to a point outside arena in direction of q
-    ray_sensor_front = LineString([(x, y), (x + cos(q) * 2 * W, y + sin(q) * 2 * H)])  
-    ray_sensor_right = LineString([(x, y), (x + cos(q + (pi / 3)) * 2 * W, y + sin(q + (pi / 3)) * 2 * H)])  
-    #print(ray_sensor_left)
-
-    sensor_front = world.intersection(ray_sensor_front)
-    sensor_right = world.intersection(ray_sensor_right)
-
-    # distance to wall from sensors
-    # distance = sqrt((sensor_front.x-x)**2+(sensor_front.y-y)**2)*100
-    distance_sensor_front = sqrt(((sensor_front.x - x) ** 2 + (sensor_front.y - y) ** 2))
-    distance_sensor_right = sqrt(((sensor_right.x - x) ** 2 + (sensor_right.y - y) ** 2))
-      
-    #simple controller - change direction of wheels every 10 seconds (100*robot_timestep) unless close to wall then turn on spot
-    if cnt%100==0:            
-        if (distance_sensor_front < 0.15 and distance_sensor_right < 0.15):
-            left_wheel_velocity = -L*pi/4
-            right_wheel_velocity = L*pi/4
-        else:                
-            left_wheel_velocity = 10/(2*pi)
-            right_wheel_velocity = 10/(2*pi)
-        
+    #Get distance to wall for both front and right sensor
+    distance_sensor_front = distanceSensorFront()
+    distance_sensor_right = distanceSensorRight()
+    
+    #benchWarmer(distance_sensor_front, distance_sensor_right)
+    #showColor()
+    #findDancePartner(cnt, distance_sensor_front)
+    #danceMoves()   
     #step simulation
     simulationstep()
 
