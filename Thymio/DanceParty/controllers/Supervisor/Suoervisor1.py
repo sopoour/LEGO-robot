a04e04e856dda680065892639cdca828fba42a5d
Jonasivers <jonas.iversminde@gmail.com>
2019-11-06 09:05:36 +0100

    Supervisor

---
+++ Thymio/DanceParty/controllers/Supervisor/Suoervisor1.py
@@ -0,0 +1,67 @@
+# Copyright 1996-2019 Cyberbotics Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""
+This controller gives to its node the following behavior:
+Listen the keyboard. According to the pressed key, send a
+message through an emitter or handle the position of Robot1.
+"""
+
+from controller import Supervisor
+
+
+class Driver (Supervisor):
+    timeStep = 64
+    x = 0.1
+    z = 0.3
+    translation = [x, 0.0, z]
+
+    def __init__(self):
+        super(Driver, self).__init__()
+        self.emitter = self.getEmitter('emitter')
+        robot = self.getFromDef('ThymioII')
+        robot2 = self.getFromDef('ThymioII_2')
+        self.translationField = robot.getField('translation')
+        self.translationField2 = robot2.getField('translation')
+        self.keyboard.enable(Driver.timeStep)
+        self.keyboard = self.getKeyboard()
+
+    def run(self):
+        # Main loop.
+        while True:
+            # Deal with the pressed keyboard key.
+            translationValues = self.translationField.getSFVec3f()
+            print('ROBOT1 is located at (' + str(translationValues[0]) + ',' + str(translationValues[2]) + ')')
+            translationValues2 = self.translationField2.getSFVec3f()
+            print('ROBOT12 is located at (' + str(translationValues2[0]) + ',' + str(translationValues2[2]) + ')')
+          # Perform a simulation step, quit the loop when
+           # Webots is about to quit.
+            if self.step(self.timeStep) == -1:
+             break
+
+    def displayHelp(self):
+        print(
+            'Commands:\n'
+            ' I for displaying the commands\n'
+            ' A for avoid obstacles\n'
+            ' F for move forward\n'
+            ' S for stop\n'
+            ' T for turn\n'
+            ' R for positioning ROBOT1 at (0.1,0.3)\n'
+            ' G for knowing the (x,z) position of ROBOT1'
+        )
+
+
+controller = Driver()
+controller.run()
---
+++ Thymio/DanceParty/controllers/controller_thymio/controller_thymio.py
@@ -0,0 +1,61 @@
+
+
+from controller import Robot, Motor
+
+TIME_STEP = 64
+
+MAX_SPEED = 6.28
+
+# distance to the wall
+dist = 12
+
+# create the Robot instance.
+robot = Robot()
+
+# See Thymio specific Webots names: https://cyberbotics.com/doc/guide/thymio2#thymio2-wbt
+# See "robot" specific functions: https://cyberbotics.com/doc/reference/robot?tab=python#getlightsensor
+# Motors
+leftMotor = robot.getMotor('motor.left')
+rightMotor = robot.getMotor('motor.right')
+leftMotor.setPosition(float('inf'))
+rightMotor.setPosition(float('inf'))
+leftMotor.setVelocity(0)
+rightMotor.setVelocity(0)
+
+# see functions of DistanceSensor: https://cyberbotics.com/doc/reference/distancesensor#wb_distance_sensor_enable
+# sensors
+sens0 = robot.getDistanceSensor("prox.horizontal.0")
+sens1 = robot.getDistanceSensor("prox.horizontal.1")
+sens2 = robot.getDistanceSensor("prox.horizontal.2")
+sens3 = robot.getDistanceSensor("prox.horizontal.3")
+sens4 = robot.getDistanceSensor("prox.horizontal.4")
+# enable distance sensor measurements in sampling period of TIME_STEP
+sens0.enable(TIME_STEP)
+sens1.enable(TIME_STEP)
+sens2.enable(TIME_STEP)
+sens3.enable(TIME_STEP)
+sens4.enable(TIME_STEP)
+
+# Wall following algorithm
+
+while robot.step(TIME_STEP) != -1:
+    # get the values read by the sensor
+    sens0_dist = sens0.getValue()
+    sens1_dist = sens1.getValue()
+    sens2_dist = sens2.getValue()
+    sens3_dist = sens3.getValue()
+    sens4_dist = sens4.getValue()
+
+    if sens0_dist > dist or sens1_dist > dist and sens2_dist > dist:
+        # Turn right
+        leftMotor.setVelocity(dist * 3.14159265359 / 4)
+        rightMotor.setVelocity(-dist * 3.14159265359 / 4)
+
+    elif sens2_dist > dist and sens3_dist > dist or sens4_dist > dist:
+        # Turn left
+        leftMotor.setVelocity(-dist * 3.14159265359 / 4)
+        rightMotor.setVelocity(dist * 3.14159265359 / 4)
+
+    else:
+        leftMotor.setVelocity(0.1 * MAX_SPEED)
+        rightMotor.setVelocity(0.1 * MAX_SPEED)
